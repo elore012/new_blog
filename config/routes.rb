69378a08bbb65aa6c9d110c9289a3dfbd184bcb3
Eleazar Lorea <azarlorea1990@gmail.com>
2021-07-09 19:59:35 +0800

    modify root

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: 'experiences#index'
-
+  root to: 'myexperiences#index'
   get '/myexperiences' => 'myexperiences#index', as:'view'
   get '/myexperiences/new' => 'myexperiences#new', as:'get_form'
   post '/myexperiences' => 'myexperiences#create', as: 'create'
   get '/myexperiences/:id' => 'myexperiences#show',as:'show'
-   get '/myexperiences/:id/edit' => 'myexperiences#edit', as:'edit'
- delete '/myexperiences/:id' => 'myexperiences#destroy', as: 'destroy'
+  get '/myexperiences/:id/edit' => 'myexperiences#edit', as:'edit'
+  delete '/myexperiences/:id' => 'myexperiences#destroy', as: 'destroy'
   post '/myexperiences/:id/edit' => 'myexperiences#update', as:'update'
    
 end
